c1ab6d3c9f546cad81ddac40d11ff46ef52dfaf0
Antonio Pantoja <tato_fos@outlook.com>
2024-04-15 21:03:02 -0400

    42. Add counter functionality
    - Added counter using useState
    - Removed hardcoded a element on Sidebar

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -62,18 +62,6 @@ export const Sidebar = () => {
           ))
         }
 
-        <a href="#" className="w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3 hover:bg-white/5 transition ease-linear duration-150">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
-            </svg>                      
-          </div>
-          <div className="flex flex-col">
-            <span className="text-lg text-slate-300 font-bold leading-5">Counter</span>
-            <span className="text-sm text-slate-500 hidden md:block">Local State</span>
-          </div>
-        </a>
-
       </div>
     </div>
   )
--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,7 +1,50 @@
+'use client'
+
+import { useState } from 'react';
+import style from './counter.page.module.css';
+
+enum Operation {
+  Add,
+  Substract
+}
+
 export default function CounterPage() {
+
+  const [counter, useCounter] = useState(0);
+
+  const handleOperationBy = (operation: Operation) => {
+    switch (operation) {
+      case Operation.Add:
+        useCounter(counter+1);
+        break;
+
+      case Operation.Substract:
+        useCounter(counter-1);
+        break;
+
+      default:
+        break;
+    }
+
+  }
+  
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className={ style['counter-page'] }>
+      <span>Shopping cart products</span>
+      <span className={ style.counter }>{ counter }</span>
+
+      <div className='flex'>
+        <button 
+          type="button"
+          className={ style.button }
+          onClick={ () => handleOperationBy(Operation.Add) }
+        >+1</button>
+        <button 
+          type="button"
+          className={ style.button }
+          onClick={ () => handleOperationBy(Operation.Substract) }
+        >-1</button>
+      </div>
     </div>
   );
 }
