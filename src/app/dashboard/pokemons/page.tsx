4dd021957bf58470566dd4965de3567b222df399
Antonio Pantoja <tato_fos@outlook.com>
2024-04-17 20:04:09 -0400

    51. Fetch pokemons funcionality

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,7 +1,17 @@
-export default function PokemonsPage() {
+const getPokemons = async (limit = 20, offset = 0) => {
+  const URL = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+  const data = await fetch(URL)
+                      .then( res => res.json());
+  return data;
+}
+
+export default async function PokemonsPage() {
+
+  const pokemons = await getPokemons();
+
   return (
     <div>
-      <h1>Hello Page Pokemons</h1>
+      { JSON.stringify(pokemons) }
     </div>
   );
 }
