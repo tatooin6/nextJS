11449600206f76133c5a1fe6042222ae0a47b12a
Antonio Pantoja <tato_fos@outlook.com>
2024-04-19 00:32:51 -0400

    53. Add pokemon card functionality

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,5 +1,4 @@
-import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
-import Image from 'next/image';
+import { PokemonGrid, PokemonsResponse, SimplePokemon } from "@/app/pokemons";
 
 const getPokemons = async (limit = 20, offset = 0):Promise<SimplePokemon[]> => {
   const URL = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
@@ -20,21 +19,9 @@ export default async function PokemonsPage() {
 
   return (
     <div className="flex flex-col">
-      <div className="flex flex-wrap gap-10 items-center justify-center">
-        {pokemons.map( pokemon => (
-          <>
-            <Image 
-              key={ pokemon.id }
-              src = {`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
-              width = {100}
-              height = {100}
-              alt = { pokemon.name }
-            />
-            <span>{ pokemon.name }</span>
-          </>
-        ))
-        }
-      </div>
+      <span className="text-5xl my-2"><small>Static</small> Pokemon List</span>
+      <PokemonGrid pokemons={pokemons} />
+      <span>pokemon</span>
     </div>
   );
 }
--- src/app/pokemons/index.ts
+++ src/app/pokemons/index.ts
@@ -1,3 +1,5 @@
+export { PokemonGrid } from './components/PokemonGrid';
+
 // exporting type because:
 // Re-exporting a type when 'isolatedModules' is enabled requires using 'export type'. [1205]
 export type { SimplePokemon } from './interfaces/simple-pokemon';
