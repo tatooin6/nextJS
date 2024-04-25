82b42bb64731c838cd6e246a387f3d9450461381
Antonio Pantoja <tato_fos@outlook.com>
2024-04-24 21:22:48 -0400

    57. Loading pokemon information by id

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,15 +1,31 @@
+import { Pokemon } from "@/pokemons";
+
 interface PageProps {
   params: { id: string; };
 }
 
-export default function PokemonPage({ params }:PageProps) {
+const getPokemon = async (id:string): Promise<Pokemon> => {
 
-  console.log('%cProps', 'color:red');
-  console.log(params.id);
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`,
+    {cache: 'force-cache'} //TODO: check later
+  ).then( res => res.json());
+
+  console.log('Se cargo a ', pokemon.name)
+
+  return pokemon;
+}
+
+export default async function PokemonPage({ params }:PageProps) {
+
+  const pokemon = await getPokemon(params.id);
+  // console.log(pokemon);
 
   return (
     <div>
       <h1>Pokemon {params.id}</h1>
+      <div>
+        { JSON.stringify(pokemon) }
+      </div>
     </div>
   );
 }
--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -4,3 +4,4 @@ export { PokemonGrid } from './components/PokemonGrid';
 // Re-exporting a type when 'isolatedModules' is enabled requires using 'export type'. [1205]
 export type { SimplePokemon } from './interfaces/simple-pokemon';
 export type { PokemonsResponse  } from './interfaces/pokemons-response';
+export type { Pokemon } from './interfaces/pokemon';
