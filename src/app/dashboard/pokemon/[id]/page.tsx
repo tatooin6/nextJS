07adfe7ab8f486dd6c320b22ee906aa632013e34
Antonio Pantoja <tato_fos@outlook.com>
2024-04-24 21:45:43 -0400

    58. Add dynamic metadata

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,13 +1,30 @@
 import { Pokemon } from "@/pokemons";
+import { Metadata } from "next";
 
 interface PageProps {
   params: { id: string; };
 }
 
+// This function is not going to be called again
+// only if a brand new page is created
+export async function generateMetadata({ params }: PageProps):Promise<Metadata> {
+
+  const { id, name } = await getPokemon(params.id);
+
+  return {
+    title: `# ${id} - ${name}`,
+    description: `This is the ${ name } page.` 
+  }
+}
+
 const getPokemon = async (id:string): Promise<Pokemon> => {
 
   const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`,
-    {cache: 'force-cache'} //TODO: check later
+    {cache: 'force-cache'}, //TODO: check later
+    // The next code revalidates the page each 6 months
+    // next: {
+    //   revalidate: 60 * 60 * 30 * 6
+    // }
   ).then( res => res.json());
 
   console.log('Se cargo a ', pokemon.name)
@@ -24,7 +41,7 @@ export default async function PokemonPage({ params }:PageProps) {
     <div>
       <h1>Pokemon {params.id}</h1>
       <div>
-        { JSON.stringify(pokemon) }
+        { pokemon.name }
       </div>
     </div>
   );
