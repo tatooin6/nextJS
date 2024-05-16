e6cd29c9ccf4cffddd449b43dda91863362c975b
Antonio Pantoja <tato_fos@outlook.com>
2024-05-15 21:57:50 -0400

    79. Add redux toolkit
    - Added library.
    - Created Providers store functional component for redux Provider.
    - Used use client on Providers.
    - Added Providers functional component to general layout.
    - Added metadata for dashboard main page.

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,5 +1,10 @@
 import { SimpleWidget } from "@/components";
 
+export const metadata = {
+  title: 'Admin Dashboard',
+  description: 'Admin Dashboard content.',
+};
+
 export default function MainPage() {
   return (
     <div className="text-black p-2">
---
+++ src/store/Providers.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { Provider } from 'react-redux'
+import { store } from './';
+
+interface ProviderProps {
+  children: React.ReactNode;
+}
+
+export const Providers = ({ children }: ProviderProps) => {
+  return (
+    <Provider store={store}>
+      { children }
+    </Provider>
+  )
+}
