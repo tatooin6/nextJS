57d8abbee81b6a5070186e44fdf35785654d8518
Antonio Pantoja <tato_fos@outlook.com>
2024-05-17 20:18:12 -0400

    81. Add useDispatch and useSelector

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
+import { useDispatch, useSelector } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './counter/counterSlice'
 
@@ -11,3 +12,7 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
